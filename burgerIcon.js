463a73f38ac71fd63a35a93ef6b723626a9dc201
jylaa <janet.amenyo@outlook.com>
2026-08-21 16:19:51 +0000

    Add files via upload

---
+++ burgerIcon.js
@@ -0,0 +1,20 @@
+
+// Burger Icon
+function toggleMenu() {
+
+    const navMain = document.querySelector('.nav-main');
+    const burgerIcon = document.querySelector('.burger i');
+
+    navMain.classList.toggle('active');
+
+    // change icon: bars <-> X
+    if (burgerIcon.classList.contains('fa-bars')) {
+
+        burgerIcon.classList.remove('fa-bars');
+        burgerIcon.classList.add("fa-xmarsk");
+
+    } else {
+        burgerIcon.classList.remove("fa-xmark");
+        burgerIcon.classList.add(fa-bars);
+    }
+}
